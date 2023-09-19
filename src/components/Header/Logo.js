bff8fcfcf81c0c74ad02ccc7fef862e8dfd92b79
Zeous_Shrey <shrey.sadhukhan21@gmail.com>
2023-09-19 08:55:20 +0530

    shrey

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,11 +5,11 @@ import Img from "@/public/profile-img.png"
 
 const Logo = () => {
   return (
-    <Link href="/">
-        <div>
-            <Image src={Img} alt='shrey' className='w-full rounded-full h-auto'/>
+    <Link href="/" className='flex items-center text-dark dark:text-light'>
+        <div className='w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4'>
+            <Image src={Img} alt='shrey' className='w-full rounded-full h-auto' sizes='20vw' priority/>
         </div>
-        <span>Shrey | Blogs</span>
+        <span className='font-bold dark:font-semibold text-lg md:text-xl'>Shrey | Blogs</span>
     </Link>
   )
 }
